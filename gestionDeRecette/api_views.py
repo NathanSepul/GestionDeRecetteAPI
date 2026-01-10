b08f122fc0013eaf161b8bceb6df7f274351cf74
NathanSepul <sepul.nathan@gmail.com>
2026-01-10 10:54:30 +0100

    decoration

--- gestionDeRecette/api_views.py
+++ gestionDeRecette/api_views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseForbidden
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -37,6 +38,8 @@ class MyTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def serve_image(request, path):
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
