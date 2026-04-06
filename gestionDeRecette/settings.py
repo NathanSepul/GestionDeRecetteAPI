4e93900e72a317998a70615c7e22caa1f3cbd788
NathanSepul <sepul.nathan@gmail.com>
2026-04-06 20:05:08 +0200

    verif

--- gestionDeRecette/settings.py
+++ gestionDeRecette/settings.py
@@ -210,8 +210,8 @@ REST_REGISTRATION = {
     'REGISTER_EMAIL_VERIFICATION_ENABLED': False,
     'RESET_PASSWORD_VERIFICATION_ENABLED': True,
 
-    'REGISTER_VERIFICATION_URL': 'sepul.be/api/user/registration/verify-email/',
-    'RESET_PASSWORD_VERIFICATION_URL': 'sepul.be/api/user/reset-password/formulaire/',
+    'REGISTER_VERIFICATION_URL': 'https://sepul.be/api/user/registration/verify-email/',
+    'RESET_PASSWORD_VERIFICATION_URL': 'https://sepul.be/api/user/reset-password/formulaire/',
 
     'VERIFICATION_FROM_EMAIL': 'nathan@sepul.be',
 
