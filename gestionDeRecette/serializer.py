ae6ca5ad0ba36a3bf42c5de1b0019b4232ebf54a
NathanSepul <sepul.nathan@gmail.com>
2026-07-20 21:55:35 +0200

    pagination

--- gestionDeRecette/serializer.py
+++ gestionDeRecette/serializer.py
@@ -1,43 +1,43 @@
-
+import math
 from rest_framework import pagination
 from rest_framework.response import Response
-from django.shortcuts import redirect
-from rest_framework.pagination import CursorPagination
-from django.http import QueryDict
-import math
+from django.core.paginator import EmptyPage
 
 class MyPagination(pagination.PageNumberPagination):
-
     page_size_query_param = 'pageSize'
     
     def get_paginated_response(self, data):
+        
         return Response({
             'next': self.page.has_next(),
             'previous': self.page.has_previous(),
             'current': self.page.number,
-            'count': self.page.paginator.count - 1,
-            'results': data})
+            'count': len(data) - 1,
+            'nbrTotalResult': self.page.paginator.count,
+            'results': data
+        })
     
     def paginate_queryset(self, queryset, request, view=None):
-        
-        page_number = request.query_params.get('page',1)
+        self.page_size = self.get_page_size(request)
+        if not self.page_size:
+            return None
 
-        # Vérification que page_number est une chaîne de caractères
-        if not isinstance(page_number, str):
-            page_number=1
+        page_number = request.query_params.get('page', '1')
 
-        # Vérification que page_number est un nombre entier valide
-        elif not page_number.isdigit() or int(page_number) <= 0:
-            page_number=1
-
-        last_page = math.ceil(queryset.count() / self.page_size) 
-        
-        if last_page != 0 and int(page_number) > last_page:
-            page_number = last_page
+        if not isinstance(page_number, str) or not page_number.isdigit() or int(page_number) <= 0:
+            page_number = '1'
 
         request.query_params._mutable = True
         request.query_params['page'] = page_number
         request.query_params._mutable = False
-        
-        return super().paginate_queryset( queryset, request, view)
-    
+
+        try:
+            return super().paginate_queryset(queryset, request, view)
+        except EmptyPage:
+            last_page = self.django_paginator.num_pages
+            
+            request.query_params._mutable = True
+            request.query_params['page'] = str(last_page if last_page > 0 else 1)
+            request.query_params._mutable = False
+            
+            return super().paginate_queryset(queryset, request, view)
